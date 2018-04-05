6ad90504631c06edd76808bde31c787bce9c3013
Багрецов Олег <bagretsov_oi@taximaxim.ru>
2018-04-05 13:41:54 +0500

    difference in non-letter symbols

--- namesimilarity.py
+++ namesimilarity.py
@@ -1,23 +1,98 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
+import difflib
+import re
 
-class nameSimilarity:
 
-    def __init__(self):
-        self.uncertainty = None
+class NameSimilarity:
 
-        self.name = None
-        self.name_init = None
+    def __init__(self, name, check_name):
+        print('\n', name, check_name)
+        self.factors = {
+            'non_letter': 3
+        }
 
-        self.check_name = None
-        self.check_name_init = None
+        self.uncertainty = 1
 
-    def check(self, name, check_name):
-        if name == check_name:
-            self.uncertainty = 0
-        else:
+        self.name = name
+        self.name_init = name
+
+        self.check_name = check_name
+        self.check_name_init = check_name
+
+    def check(self):
+        if self.name != self.check_name:
+            self.check_diff_non_letter()
+
+        if self.name != self.check_name:
             self.uncertainty = 100
+
+        if self.uncertainty == 1:
+            self.uncertainty = 0
 
         similarity = 100 - self.uncertainty
         return similarity
+
+    def check_diff_non_letter(self):
+        self.check_diff_base(
+            replace_condition=self.non_letter
+        )
+
+    def check_diff_base(
+        self,
+        replace_condition=None,
+        insert_condition=None,
+        delete_condition=None
+    ):
+        conditions = {
+            'equal': None,
+            'replace': replace_condition,
+            'insert': insert_condition,
+            'delete': delete_condition
+        }
+
+        check_name_tmp = ''
+        sequence = difflib.SequenceMatcher(None, self.name, self.check_name)
+        for diff in sequence.get_opcodes():
+
+            operation,\
+                sub_str_index_start_name, sub_str_index_end_name,\
+                sub_str_index_start_check_name, sub_str_index_end_check_name\
+                = diff
+
+            name_sub_str = self.name[
+                sub_str_index_start_name:sub_str_index_end_name
+            ]
+            check_name_sub_str = self.check_name[
+                sub_str_index_start_check_name:sub_str_index_end_check_name
+            ]
+
+            try:
+                # если условие корректно ...
+                if (
+                    conditions[operation] is not None
+                    and
+                    conditions[operation](name_sub_str, check_name_sub_str)
+                ):
+                    # ... зменяем подстроку для проверяемого имени
+                    # на подстроку имени
+                    check_name_tmp += name_sub_str
+                else:
+                    # ... иначе берём подстроку из проверяемого имени
+                    check_name_tmp += check_name_sub_str
+            except KeyError as e:
+                raise ValueError('Undefined operation: {}'.format(e.args[0]))
+
+        self.check_name = check_name_tmp
+
+    def non_letter(self, a, b):
+        if (
+            re.match('^[^\w]*$', a) is not None
+            and
+            re.match('^[^\w]*$', b) is not None
+        ):
+            self.uncertainty *= self.factors['non_letter']
+            return True
+
+        return False
--- tests.py
+++ tests.py
@@ -2,10 +2,28 @@
 # -*- coding: UTF-8 -*-
 
 import unittest
-from namesimilarity import nameSimilarity
+from namesimilarity import NameSimilarity
 
 
 class Names(unittest.TestCase):
 
+    # имена одинаковы
     def test_equals(self):
-        self.assertEqual(nameSimilarity().check('Гримо Мишель', 'Гримо Мишель'), 100)
+        self.assertEqual(NameSimilarity(
+            "Гримо Мишель",
+            "Гримо Мишель"
+        ).check(), 100)
+
+    # имена разные
+    def test_non_equals(self):
+        self.assertEqual(NameSimilarity(
+            "Пушкин Александр Сергеевич",
+            "Толстой Лев Николаевич"
+        ).check(), 0)
+
+    # имена похожи, отличаются символом апострофа
+    def test_non_letter_symbol(self):
+        self.assertEqual(NameSimilarity(
+            "О'Лири Патрик",
+            "О’Лири Патрик"
+        ).check(), 97)
